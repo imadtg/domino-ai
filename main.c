7725e224ef14eb28623435dd181973da7a9d4084
imadtg <i.haidi@outlook.com>
2023-06-01 11:50:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ void start(){
     int n, cant_pass, ai_play, skip, depth;
     float (*ai_function)(Game *, int, int, int *);
     enum Mode ai_mode;
-    Move moves[MAX_NUM_PLY_MOVE], move;
+    Move moves[DCOUNT], move;
     do {
         print_game(g);
         get_playing_moves(g, moves, &n, &cant_pass);
